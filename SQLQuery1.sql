31a163aa85561092b3654fc5dde647073ee57148
Sunny <sunnysejwarvitm005@gmail.com>
2022-06-20 23:19:44 -1200

    [Sunny] Add : Create Address Book Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,11 @@
 create database AddressBook_Service
+create table AddressBook_Table
+(ID int IDENTITY(1,1) Primary Key,
+FirstName varchar(15)Not Null,
+LastName varchar(15)Not Null,
+Address varchar(50)Not Null,
+City varchar(10)Not Null,
+State varchar(10) Not Null,
+ZipCode Bigint Not Null,
+PhoneNumber Bigint Not Null,
+Email varchar(20) Not Null)
